06eeea3e7e5784c02aab1e8c4132586f851e1f49
DuminduTraa <dumindutissera@gmail.com>
2017-01-13 16:50:32 +0800

    Initial commit

---
+++ app/src/main/java/com/dumindudulanga/cwb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dumindudulanga.cwb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
