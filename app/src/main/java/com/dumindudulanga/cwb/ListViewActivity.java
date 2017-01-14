997e8c24bf6f562bd3bae2581ae4945b70fa5678
DuminduTraa <dumindutissera@gmail.com>
2017-01-14 14:21:55 +0800

    Firebase connect, location display in listView

--- app/src/main/java/com/dumindudulanga/cwb/ListViewActivity.java
+++ app/src/main/java/com/dumindudulanga/cwb/ListViewActivity.java
@@ -16,26 +16,15 @@ import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity {
 
-    public ListView listView;
-
-    ArrayList<String> placeNames = new ArrayList<String>();
-
-    boolean isDone = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
-        listView = (ListView)findViewById(R.id.list_view);
 
-
-        //placeNames.add("Place A");
-        //placeNames.add("Place B");
-        //placeNames.add("place C");
+        final ArrayList<String> placeNames = new ArrayList<String>();
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-
         DatabaseReference myRef = database.getReference("CarWashBay");
         //DatabaseReference carWashRef = myRef.child("CarWashBay");
 
@@ -46,40 +35,23 @@ public class ListViewActivity extends AppCompatActivity {
                 Iterable<DataSnapshot> iterator = dataSnapshot.getChildren();
 
                 for (DataSnapshot child : iterator){
-                    //
-                    placeNames.add(child.child("stName")+"");
-                    Log.d("Children",placeNames.get(0)+"j");
-
+                    placeNames.add(child.child("stName").getValue().toString());
+                    Log.d("Children",placeNames.get(0)+"");
                 }
-                isDone = true;
+
+                ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(ListViewActivity.this,
+                        android.R.layout.simple_expandable_list_item_1, placeNames);
+
+                ListView listView = (ListView)findViewById(R.id.list_view);
+                listView.setAdapter(itemsAdapter);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
-
         });
-
-
-
-
-
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        while (!isDone){
 
-        }
-
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_expandable_list_item_1, placeNames);
-        listView.setAdapter(itemsAdapter);
-
-        //Log.e("dfd",placeNames.get(0)+"");
-
-    }
 }
